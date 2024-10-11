db06771af2e308399f19032654f908ad2d8b214e
EVikramss <156318649+EVikramss@users.noreply.github.com>
2024-10-11 11:16:06 +0530

    Update setupEnv.sh

--- scripts/envSetup/setupEnv.sh
+++ scripts/envSetup/setupEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #run aws configure before invoking script
 account_id=$(aws sts get-caller-identity --query 'Account' --output text)
-if [ -z "$HOME/.aws/credentials" ]; then
+if [ ! -f "$HOME/.aws/credentials" ]; then
     echo "run aws configure"
     exit
 fi
