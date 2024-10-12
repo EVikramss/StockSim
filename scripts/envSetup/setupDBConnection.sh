ea14813f08fb8d70b269adc60dfc50d6c535696b
evikramss <evikramss@gmail.com>
2024-10-12 13:06:24 +0000

    updated

--- scripts/envSetup/setupDBConnection.sh
+++ scripts/envSetup/setupDBConnection.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # References : https://dev.to/bensooraj/accessing-amazon-rds-from-aws-eks-2pc3#lets-build-the-bridge
 
 account_id=$(aws sts get-caller-identity --query 'Account' --output text)
@@ -18,13 +17,13 @@ else
 	rdc_endpoint=$(aws rds describe-db-instances --query "DBInstances[0].Endpoint.Address" --output text)
 	rdc_port=$(aws rds describe-db-instances --query "DBInstances[0].Endpoint.Port" --output text)
 
-	if [ -z "$rds_vpc_id" ]; then
+	if [ ! -z "$rds_vpc_id" ]; then
 		vpcPeeringConnectionId=$(aws ec2 create-vpc-peering-connection --vpc-id $cluster_vpc_id --peer-vpc-id $rds_vpc_id --query "VpcPeeringConnection.VpcPeeringConnectionId" --output text)
 
 		while true; do
 			output=$(aws ec2 describe-vpc-peering-connections --vpc-peering-connection-ids $vpcPeeringConnectionId --query "VpcPeeringConnections[0].Status.Code" --output text)
 		
-			if [ "$output" = "pending-acceptance" ]; then
+			if [ "$output" = "pending-acceptance" ] || [ "$output" = "active" ]; then
 				echo 'provide acceptance'
 			break
 			fi
@@ -33,7 +32,7 @@ else
 			sleep 10
 		done
 
-		aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id $vpcPeeringConnectionId
+		acceptance=(aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id $vpcPeeringConnectionId --output text)
 
 		while true; do
 			output=$(aws ec2 describe-vpc-peering-connections --vpc-peering-connection-ids $vpcPeeringConnectionId --query "VpcPeeringConnections[0].Status.Code" --output text)
@@ -51,13 +50,13 @@ else
 		cluster_route_table_id=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$cluster_vpc_id" --query "RouteTables[0].RouteTableId" --output text)
 
 		if [ -z "$rdc_route_table_id" ] && [ -z "$cluster_route_table_id" ]; then
+			echo "Unable to fetch route tables"
+		else
 			aws ec2 create-route --route-table-id $rdc_route_table_id --destination-cidr-block $cluster_vpc_cider_block --vpc-peering-connection-id $vpcPeeringConnectionId
 			aws ec2 create-route --route-table-id $cluster_route_table_id --destination-cidr-block $rdc_vpc_cider_block --vpc-peering-connection-id $vpcPeeringConnectionId
 			
 			# add security group 
 			aws ec2 authorize-security-group-ingress --group-id ${rdc_vpc_security_group_id} --protocol tcp --port $rdc_port --cidr $cluster_vpc_cider_block
-			
-			if [ -z "$rdc_endpoint" ]; then
 			
 echo "
 apiVersion: v1
@@ -74,13 +73,6 @@ spec:
 status:
   loadBalancer: {}
 " | kubectl apply -f -
-
-			else
-				echo "unable to fetch rds endpoint"
-			fi
-		else
-			echo "Unable to fetch routeID for VPC's."
 		fi
-
 	fi
 fi
